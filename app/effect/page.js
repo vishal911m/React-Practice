cd9b056e3d52007e6504cfcaa4b2f9a70b67feb7
vishal911m <vishal911m@gmail.com>
2025-11-07 19:48:43 +0530

    Axios API

--- app/effect/page.js
+++ app/effect/page.js
@@ -1,19 +1,27 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Users() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(res => res.json())
-      .then(data => setUsers(data));
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+        setUsers(res.data);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+
+    fetchUsers();
   }, []);
 
   return (
     <ul>
-      {users.map(user => (
+      {users.map((user) => (
         <li key={user.id}>{user.name}</li>
       ))}
     </ul>
