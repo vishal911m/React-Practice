14098f550503d56057a4957d0db3d1598ab2b1aa
vishal911m <vishal911m@gmail.com>
2025-11-07 20:21:15 +0530

    Axios API

--- app/conditionalrendering/page.js
+++ app/conditionalrendering/page.js
@@ -1,14 +1,22 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function FetchData() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts/1")
-      .then(res => res.json())
-      .then(setData);
+    const fetchPost = async () => {
+      try {
+        const res = await axios.get("https://jsonplaceholder.typicode.com/posts/1");
+        setData(res.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchPost();
   }, []);
 
   if (!data) return <p>Loading...</p>;
