1e74e2aa7d38e77692df222b5150a7eb5889ef12
vishal911m <vishal911m@gmail.com>
2025-11-10 17:31:45 +0530

    Flex box

---
+++ app/flex/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div class="relative w-[400px] h-[300px] border-2 border-gray-700 bg-gray-100">
+      <div class="absolute w-1/4 h-1/4 bottom-0 right-0 bg-blue-600 text-white flex items-center justify-center rounded-md">
+        div2
+      </div>
+    </div>
+  )
+}
+
+export default page
