424738d3a1ebaef4777b503ce4b5d2180ca0b2d7
vishal911m <vishal911m@gmail.com>
2025-11-22 14:39:59 +0530

    Windowing: Completed

--- app/windowing/page.js
+++ app/windowing/page.js
@@ -1,29 +1,42 @@
 "use client"
 import React, { useState } from 'react'
+import { FixedSizeList  } from "react-window";
 
 const Page = () => {
   const [load, setLoad] = useState(false);
 
-  const renderList = ()=>{
-    return new Array(10000).fill(0).map((_,i)=>(
-      <h3
-        key={i}
+  const Row = ({index, style})=>(
+    <h3
+        key={index}
         style={{
+          ...style,
           margin:0,
           padding:12,
-          background: i % 2 === 0 ? "#66aaff" : "#001e80",
+          background: index % 2 === 0 ? "#66aaff" : "#001e80",
           color: 'white'
         }}
         className='list'
       >
-        Block{i}
+        Block{index}
       </h3>
-    ))
+  )
+
+  const renderList = ()=>{
+    return (
+    <FixedSizeList 
+      height={500}
+      itemCount={10000}
+      itemSize={50}
+      width={500}
+    >
+      {Row}
+    </FixedSizeList >
+  );
   }
   return (
     <>
       <h1 style={{textAlign: 'center'}}>Windowing Example</h1>
-      {/* <div className='App'
+      <div className='App'
         style={{
           width: "500px",
           height: "500px",
@@ -31,17 +44,7 @@ const Page = () => {
           margin: "0 auto",
           overflow: "auto",
         }}
-      >{load && renderList()}</div> */}
-      <div 
-        className='App'
-        style={{
-          width: "500px",
-          height: "500px",
-          border: "1px solid black",
-          margin: "0 auto",
-          overflow: "auto",
-        }}
-      >{renderList()}</div>
+      >{load && renderList()}</div>
       <div style={{display: 'flex', justifyContent: 'center'}}>
         <button style={{textAlign: 'center'}} onClick={()=>setLoad(true)}>
           Load List
