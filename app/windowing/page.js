d1c96a7c5bcba71ccc02a67e619e39bf8c40dee1
vishal911m <vishal911m@gmail.com>
2025-11-22 13:40:43 +0530

    Windowing: Starting files

---
+++ app/windowing/page.js
@@ -0,0 +1,44 @@
+"use client"
+import React, { useState } from 'react'
+
+const Page = () => {
+  const [load, setLoad] = useState(false);
+
+  const renderList = ()=>{
+    return new Array(10000).fill(0).map((_,i)=>(
+      <h3
+        key={i}
+        style={{
+          margin:0,
+          padding:12,
+          background: i % 2 === 0 ? "#66aaff" : "#001e80",
+          color: 'white'
+        }}
+        className='list'
+      >
+        Block{i}
+      </h3>
+    ))
+  }
+  return (
+    <>
+      <h1 style={{textAlign: 'center'}}>Windowing Example</h1>
+      <div className='App'
+        style={{
+          width: "500px",
+          height: "500px",
+          border: "1px solid black",
+          margin: "0 auto",
+          overflow: "auto",
+        }}
+      >{load && renderList()}</div>
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <button style={{textAlign: 'center'}} onClick={()=>setLoad(true)}>
+          Load List
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default Page
